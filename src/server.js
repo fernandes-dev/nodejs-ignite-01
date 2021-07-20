21493c503df1453092fe093ee36432a0ff22a4d3
Eduardo Fernandes <eduardo.yugan@gmail.com>
2021-07-20 14:46:22 -0400

    fix: add port variable

--- src/server.js
+++ src/server.js
@@ -1,3 +1,5 @@
 const app = require('./');
 
-app.listen(3333, () => console.log('Server started'));
+const port = process.env.PORT || 3333;
+
+app.listen(port, () => console.log('Server started'));
